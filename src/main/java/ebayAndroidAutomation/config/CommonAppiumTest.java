5d090cc798685b871cfc44b20c44e060c3b819ff
suraj <suraj@technosnoop.com>
2018-02-07 07:32:18 +0530

    updated pom, added json parser and utils

---
+++ src/main/java/ebayAndroidAutomation/config/CommonAppiumTest.java
@@ -0,0 +1,75 @@
+package ebayAndroidAutomation.config;
+
+import io.appium.java_client.AppiumDriver;
+import io.appium.java_client.MobileElement;
+import org.apache.log4j.Logger;
+import org.openqa.selenium.By;
+import org.openqa.selenium.NoSuchElementException;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.util.Set;
+
+public class CommonAppiumTest {
+    public AppiumDriver driver;
+    Logger logger = Logger.getLogger(CommonAppiumTest.class);
+
+    public CommonAppiumTest(AppiumDriver<MobileElement> driver) {
+        this.driver = driver;
+    }
+
+    public void waitForPageToLoad(WebElement id) {
+        WebDriverWait wait = new WebDriverWait(driver, 15);
+        wait.until(ExpectedConditions.elementToBeClickable(id));
+    }
+
+    public void waitForElementToDisAppear(String id) {
+        WebDriverWait wait = new WebDriverWait(driver, 15);
+        wait.until(ExpectedConditions.invisibilityOfElementLocated(By.id(id)));
+    }
+
+    public WebElement waitForElement(WebElement arg) {
+        waitForPageToLoad(arg);
+        WebElement el = arg;
+        return el;
+    }
+    public boolean isElementPresent(By by) {
+        try {
+            driver.findElement(by);
+            return true;
+        } catch (NoSuchElementException e) {
+            return false;
+        }
+
+    }
+    /**
+     * method to set the context to required view.
+     *
+     * @param context view to be set
+     */
+    public void setContext(String context) {
+        try {
+            Thread.sleep(4000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        Set<String> contextNames = driver.getContextHandles();
+        for (String contextName : contextNames) {
+            System.out.println(contextName); //prints out something like NATIVE_APP \n WEBVIEW_1
+        }
+        driver.context((String) contextNames.toArray()[1]); // set context to WEBVIEW_1
+
+        logger.info("Current context" + driver.getContext());
+    }
+
+    public void clickBackButton() {
+        driver.navigate().back(); //Closes keyboard
+        //driver.navigate().back(); //Comes out of edit mode
+    }
+
+    public String getCurrentMethodName() {
+        return Thread.currentThread().getStackTrace()[2].getMethodName();
+    }
+
+}
