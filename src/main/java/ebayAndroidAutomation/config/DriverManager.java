78e8699861038430f41ed8ddd7a837836757c5cd
saurav7277 <sauravkumar1995@gmail.com>
2024-04-21 13:54:03 +0530

    jenkins integration changes

--- src/main/java/ebayAndroidAutomation/config/DriverManager.java
+++ src/main/java/ebayAndroidAutomation/config/DriverManager.java
@@ -1,7 +1,9 @@
 package ebayAndroidAutomation.config;
 
 import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.android.options.UiAutomator2Options;
 import org.apache.log4j.Logger;
+import org.openqa.selenium.MutableCapabilities;
 import org.openqa.selenium.remote.DesiredCapabilities;
 
 import java.io.FileInputStream;
@@ -22,46 +24,63 @@ public class DriverManager {
 
 
     public static AndroidDriver getDriver() throws IOException {
-        input = new FileInputStream("src/main/resources/config.properties");
-        prop.load(input);
-        if (prop.getProperty("platformName").equalsIgnoreCase("Android")) {
-            logger.info("Device property found for Android ");
-            AdbManager adbdriver=new AdbManager(prop.getProperty("DeviceID"));
-            boolean result=adbdriver.isAppAlradyInstalled(prop.getProperty("Package"));
-            if(result==true){
-                logger.info("App is alredy installed clearing content");
-                adbdriver.clearAppsData(prop.getProperty("Package"));
-            }
-            else{
-                adbdriver.installApp(prop.getProperty("AppPath"),prop.getProperty("Package"));
-                try {
-                    Thread.sleep(15000);
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
-                logger.info("App Installed Succesfully");
-            }
-            androidSetup();
-
-        }
+//        input = new FileInputStream("src/main/resources/config.properties");
+//        prop.load(input);
+//        if (prop.getProperty("platformName").equalsIgnoreCase("Android")) {
+//            logger.info("Device property found for Android ");
+//            AdbManager adbdriver=new AdbManager(prop.getProperty("DeviceID"));
+//            boolean result=adbdriver.isAppAlradyInstalled(prop.getProperty("Package"));
+//            if(result==true){
+//                logger.info("App is alredy installed clearing content");
+//                adbdriver.clearAppsData(prop.getProperty("Package"));
+//            }
+//            else{
+//                adbdriver.installApp(prop.getProperty("AppPath"),prop.getProperty("Package"));
+//                try {
+//                    Thread.sleep(15000);
+//                } catch (InterruptedException e) {
+//                    e.printStackTrace();
+//                }
+//                logger.info("App Installed Succesfully");
+//            }
+//            androidSetup();
+//
+//        }
+        androidSetup();
         driver.manage().timeouts().implicitlyWait(3, TimeUnit.SECONDS);
         return driver;
     }
 
     public static AndroidDriver androidSetup() throws MalformedURLException {
         logger.info("Setting Android Driver");
+//        DesiredCapabilities caps = new DesiredCapabilities();
+//        caps.setCapability("appium:deviceName", prop.getProperty("deviceName"));
+//        caps.setCapability("platformName", prop.getProperty("platformName"));
+//        caps.setCapability("appium:platformVersion", prop.getProperty("platformVersion"));
+//        caps.setCapability("appium:app", prop.getProperty("AppPath"));
+//        caps.setCapability("avd", prop.getProperty("avd"));
+//        caps.setCapability("appPackage", prop.getProperty("Package"));
+//        caps.setCapability("appActivity", prop.getProperty("Activity"));
+//        caps.setCapability("appium:automationName", prop.getProperty("automationName"));
+//        caps.setCapability("newCommandTimeout", 10000);
+//        caps.setCapability("udid" , prop.getProperty("DeviceID"));
+//        MutableCapabilities capabilities = new UiAutomator2Options();
+//        driver = new AndroidDriver(new URL("http://127.0.0.1:4723/wd/hub"),capabilities);
+
+        String userName = System.getenv("BROWSERSTACK_USERNAME");
+        String accessKey = System.getenv("BROWSERSTACK_ACCESS_KEY");
+        String browserstackLocal = System.getenv("BROWSERSTACK_LOCAL");
+        String buildName = System.getenv("BROWSERSTACK_BUILD_NAME");
+        String browserstackLocalIdentifier = System.getenv("BROWSERSTACK_LOCAL_IDENTIFIER");
+        String app = System.getenv("BROWSERSTACK_APP_ID");
         DesiredCapabilities caps = new DesiredCapabilities();
-        caps.setCapability("appium:deviceName", prop.getProperty("deviceName"));
-        caps.setCapability("platformName", prop.getProperty("platformName"));
-        caps.setCapability("appium:platformVersion", prop.getProperty("platformVersion"));
-        caps.setCapability("appium:app", prop.getProperty("AppPath"));
-        caps.setCapability("avd", prop.getProperty("avd"));
-        caps.setCapability("appPackage", prop.getProperty("Package"));
-        caps.setCapability("appActivity", prop.getProperty("Activity"));
-        caps.setCapability("appium:automationName", prop.getProperty("automationName"));
-        caps.setCapability("newCommandTimeout", 10000);
-        caps.setCapability("udid" , prop.getProperty("DeviceID"));
-        driver = new AndroidDriver(new URL("http://127.0.0.1:4723/"), caps);
+        caps.setCapability("app", app);
+        caps.setCapability("device", "Samsung Galaxy S8");
+        caps.setCapability("build", buildName);
+        caps.setCapability("browserstack.local", browserstackLocal);
+        caps.setCapability("browserstack.localIdentifier", browserstackLocalIdentifier);
+        driver = new AndroidDriver(new URL("https://"+userName+":"+accessKey+"@hub-cloud.browserstack.com/wd/hub"), caps);
+
         logger.info("Android Driver set succesfully");
         return driver;
     }
